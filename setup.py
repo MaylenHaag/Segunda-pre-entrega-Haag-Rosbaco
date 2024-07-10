b4cb6020dae9e7561cd041fd15e2ddb5c0774d93
Maylén Haag Rosbaco <maylenhaag23@gmail.com>
2024-07-10 16:33:14 -0300

    agregue el paquete distribuible

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paquete',
+    version='1.0',
+    description='Segunda pre-entrega',
+    author='Maylen Haag Rosbaco',
+    package=find_packages,
+    author_email="maylenhaag23@gmail.com"
+)
